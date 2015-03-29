2c1031f8ac8717324a10f33e9505e329d5f8eba7
Ryan Varick <git@ryanvarick.com>
2015-03-29 14:21:15 -0700

    Note that ssh-add commands must be run locally

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -6,19 +6,19 @@ if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
     ln -s "${DOTFILE_BASE}/.ssh" ~/.ssh
 fi
 
-echo '> Drop all SSH identities? (y/n)'
+echo '> Drop all SSH identities (must be run locally)? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
     ssh-add -D
 fi
 
-echo '> Unlock private key (requires 1Pw)? (y/n)'
+echo '> Unlock private key (must be run locally, requires 1Pw)? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
     ssh-add -K ~/.ssh/id_rsa
 fi
 
-echo '> Unlock Github private key (requires 1Pw)? (y/n)'
+echo '> Unlock Github private key (must be run locally, requires 1Pw)? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
     ssh-add -K ~/.ssh/github_rsa

1e5c8af8fe2630ca0f27607277c17b79bc57865c
Ryan Varick <git@ryanvarick.com>
2015-03-29 07:40:27 -0700

    Update dotfile paths

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
 
-##
-# Initial setup
-##
-
-# base paths for symlinking dotfiles (use `~/Public/` for testing)
-BASE="${HOME}/Dropbox/OSX"
-
-
 echo '> Symlink dotfiles? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 	# dotfiles
-	ln -s "${BASE}/dotfiles/bash_profile" ~/.bash_profile
-	ln -s "${BASE}/dotfiles/emacs" ~/.emacs
-	ln -s "${BASE}/dotfiles/gitconfig" ~/.gitconfig
-	ln -s "${BASE}/dotfiles/gitignore_global" ~/.gitignore_global
-	ln -s "${BASE}/dotfiles/hgignore_global" ~/.hgignore_global
+	ln -s "${DOTFILE_BASE}/.bash_profile" ~/.bash_profile
+	ln -s "${DOTFILE_BASE}/.emacs" ~/.emacs
+	ln -s "${DOTFILE_BASE}/.gitconfig" ~/.gitconfig
+	ln -s "${DOTFILE_BASE}/.gitignore_global" ~/.gitignore_global
+	ln -s "${DOTFILE_BASE}/.hgignore_global" ~/.hgignore_global
 
 	# directories
-	ln -s "${BASE}/dotfiles/atom" ~/.atom
-	ln -s "${BASE}/dotfiles/ssh" ~/.ssh
+	ln -s "${DOTFILE_BASE}/.atom" ~/.atom
 fi
 
 echo '> Symlink ~/Downloads to ~/Desktop (requires admin)? (y/n)'

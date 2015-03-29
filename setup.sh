6cd5b4ea408ec1322f788e8b44f30368ceaeb3d8
Ryan Varick <git@ryanvarick.com>
2015-03-29 08:11:39 -0700

    Reorganize around smaller scripts

--- setup.sh
+++ setup.sh
@@ -1,91 +1,67 @@
 #!/usr/bin/env bash
 
-##
-# Interactive blah balh
-##
+# base path for scripts
+DOTFILE_BASE="/Users/rvarick/Public/dotfiles"
+#DOTFILE_BASE="${HOME}/Dropbox/dotfiles"
+export DOTFILE_BASE
 
 clear
 
-echo '> Set up bash? (y/n)'
-read response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-	scripts/bash.sh
-fi
-
-echo '> Set up hostname (requires admin)? (y/n)'
-read response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-	scripts/bash.sh
-fi
-
-echo '> Symlink files (requires admin)? (y/n)'
-read response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-	scripts/symlinks.sh
-fi
-
-echo '> Set up SSH (requires admin and 1PM)? (y/n)'
-read response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-	scripts/ssh.sh
-fi
+# run scripts
+sh "${DOTFILE_BASE}/scripts/bash.sh"
+sh "${DOTFILE_BASE}/scripts/hostname.sh"
+sh "${DOTFILE_BASE}/scripts/symlinks.sh"
+sh "${DOTFILE_BASE}/scripts/ssh.sh"
 
 echo '> Set OSX defaults? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-	scripts/defaults.sh
-fi
 
+	sh "${DOTFILE_BASE}/scripts/defaults.sh"
 
+	# echo '> Open a color picker so you can manually import *.clr files? (y/n)'
+	# read response
+	# if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	# 	osascript -e 'tell application "terminal" to choose color' > /dev/null
+	# fi
+	#
+	# # TODO: Copy fonts
+	# # TODO: Copy scripts
 
-echo '> Open a color picker so you can manually import *.clr files? (y/n)'
-read response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-	osascript -e 'tell application "terminal" to choose color' > /dev/null
-fi
-
-# TODO: Copy fonts
-# TODO: Copy scripts
-
-
-
-
-
-
-
-echo
-echo 'All done!'
-echo
-echo 'Perform the following changes manually:'
-echo
-echo '- Verify FileVault is enabled in System Preferences → Security & Privacy'
-echo '- Configure VPN settings in System Preferences → Network'
-echo '- Disable guest user account in System Preferences → Users & Groups'
-echo
-echo '- Enable "Change Picture" in System Preferences → Desktop & Screensaver'
-echo '- Enable "Tap to Click" in System Preferences → Trackpad → Point & Click'
-echo '- Enable "Three finger drag" in System Preferences → Trackpad → Point & Click'
-echo '- Disable "When song changes" in iTunes Preferences → General'
-echo '- Disable "Keep iTunes Media folder organized" in iTunes Preferences → Advanced'
-echo '- Disable "Copy files..." in iTunes Preferences → Advanced'
-echo
-echo 'Install the following apps from the App Store:'
-echo
-echo '- 1Password, Clear, iA Writer Pro'
-echo '- Moneywell, Pixelmator, Sketch, Transmit'
-echo '- Number, iPhoto, Xcode'
-echo
-echo 'Install the following apps from the web:'
-echo
-echo '- Dropbox, Crashplan'
-echo '- Chrome, Firefox'
-echo '- Atom, io.js, SourceTree'
-echo '- Bartender, Fantastical, iStatsMenu (restore plist files)'
-echo '- Omnigraffle 5'
-echo
-echo '> Some changes are not applied instantly. Log out now? (y/n)'
-read response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-	# FIXME: Crashes if single user logged in?
-	osascript -e 'tell application "loginwindow" to «event aevtrlgo»'
+	echo
+	echo 'All done!'
+	echo
+	echo 'Perform the following changes manually:'
+	echo
+	echo '- Verify FileVault is enabled in System Preferences → Security & Privacy'
+	echo '- Configure VPN settings in System Preferences → Network'
+	echo '- Disable guest user account in System Preferences → Users & Groups'
+	echo
+	echo '- Enable "Change Picture" in System Preferences → Desktop & Screensaver'
+	echo '- Enable "Tap to Click" in System Preferences → Trackpad → Point & Click'
+	echo '- Enable "Three finger drag" in System Preferences → Trackpad → Point & Click'
+	echo '- Disable "When song changes" in iTunes Preferences → General'
+	echo '- Disable "Keep iTunes Media folder organized" in iTunes Preferences → Advanced'
+	echo '- Disable "Copy files..." in iTunes Preferences → Advanced'
+	echo
+	echo 'Install the following apps from the App Store:'
+	echo
+	echo '- 1Password, Clear, iA Writer Pro'
+	echo '- Moneywell, Pixelmator, Sketch, Transmit'
+	echo '- Number, iPhoto, Xcode'
+	echo
+	echo 'Install the following apps from the web:'
+	echo
+	echo '- Dropbox, Crashplan'
+	echo '- Chrome, Firefox'
+	echo '- Atom, io.js, SourceTree'
+	echo '- Bartender, Fantastical, iStatsMenu (restore plist files)'
+	echo '- Omnigraffle 5'
+	echo
+	echo '> Some changes are not applied instantly. Log out now? (y/n)'
+	read response
+	if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+		# FIXME: Crashes if single user logged in?
+		osascript -e 'tell application "loginwindow" to «event aevtrlgo»'
+	fi
 fi

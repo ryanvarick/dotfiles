8d56c3f6ab84314f65af7d3826b35d4f0b15d7d5
Ryan Varick <git@ryanvarick.com>
2016-08-17 12:35:48 -0700

    Add script to fix spotlight issues

---
+++ scripts/fixspotlight.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##
+# Spotlight: Fix spotlight cpu issue
+##
+
+IFS=$'\n'
+killspotlight="no"
+if [ -e /Library/Managed\ Preferences/com.apple.Spotlight.plist ];
+then
+    echo "System: Spotlight Managed Pref Plist found.  Removing..."
+    rm -rf /Library/Managed\ Preferences/com.apple.Spotlight.plist
+    killspotlight="yes"
+fi
+for i in `ls -d /Library/Managed\ Preferences/*/`;
+do
+    if [ -e $i/com.apple.Spotlight.plist ];
+    then
+        echo "System: Spotlight Managed Pref Plist found in $i  Removing..."
+        rm -f $i/com.apple.Spotlight.plist
+        killspotlight="yes"
+    fi
+done
+if [ $killspotlight == "yes" ]; then killall Spotlight; fi

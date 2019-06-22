4897901a9ae56ca29a0c7834c1c3f05bd50ff441
Ryan Varick <github@ryanvarick.com>
2019-06-22 15:06:09 -0700

    Add SSH config

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
+echo '> Symlink SSH config? (y/n)'
+read response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	mkdir ~/.ssh
+	ln -sf "${DOTFILE_BASE}/.ssh/config" ~/.ssh/config
+fi
+
 echo '> Drop all SSH identities (must be run locally)? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    ssh-add -D
+	ssh-add -D
 fi
 
 echo '> Unlock private key (must be run locally, requires 1Pw)? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    ssh-add -K ~/.ssh/id_rsa
+	ssh-add -K ~/.ssh/id_rsa
 fi
 
 echo '> Unlock Github private key (must be run locally, requires 1Pw)? (y/n)'
 read response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    ssh-add -K ~/.ssh/github_rsa
+	ssh-add -K ~/.ssh/github_rsa
 fi
